11d3bcaadcf0cfd8e9453b9ae0237efa7b6abc53
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-11-25 21:36:07 +0300

    4. Gün Ödev 5 Eklendi.

---
+++ 4.Gün/Homework5/Homework5/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class MyDictionary<T>
+    {
+        T[] items;
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+        public int Count
+        {
+            get { return items.Length; }
+        }
+    }
+}
---
+++ 4.Gün/Homework5/Homework5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> names = new MyDictionary<string>();
+            names.Add("Berkcan");
+            names.Add("Şevval");
+            names.Add("Nurana");
+            names.Add("Mete");
+            names.Print();
+            Console.WriteLine(names.Count);
+        }
+    }
+}
