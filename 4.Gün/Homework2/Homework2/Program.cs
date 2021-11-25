59116802914c1dfa94e5d7d3c2ca77b6482458b8
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-11-25 20:41:18 +0300

    4. Gün 2. Ödev Eklendi.

---
+++ 4.Gün/Homework2/Homework2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                ID = 1,
+                FirstName = "Berkcan",
+                LastName = "Gümüşışık",
+                City = "Ankara"
+            };
+            Customer customer2 = new Customer(2, "Engin", "Demiroğ", "Ankara");
+
+            Console.WriteLine(customer2.FirstName);
+
+        }
+
+
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
