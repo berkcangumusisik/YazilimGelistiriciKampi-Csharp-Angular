9cf2165b3d005abe67e5233d1deec71c9e469fa0
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-11-25 21:23:37 +0300

    4.Gün 3. Ödev Eklendi

---
+++ 4.Gün/Homework3/Homework3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("Ankara");
+            cities.Add("Ankara");
+            cities.Add("Ankara");
+            cities.Add("Ankara");
+
+            Console.WriteLine(cities.Count);
+            MyList<string> cities2 = new MyList<string>();
+            cities2.Add("Ankara");
+            cities2.Add("Ankara");
+            cities2.Add("Ankara");
+            cities2.Add("Ankara");
+            cities2.Add("Ankara");
+
+            Console.WriteLine(cities2.Count);
+
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
