96826e320472204e5c182f013d54729767591c07
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-12-01 17:47:29 +0300

    5.Gün 2. Ödev Eklendi.

---
+++ 5.Gün/Homework2/Homework2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+
+            IPersonManager employeeManager = new EmployeeManager();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi.");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Güncellendi.");
+        }
+    }
+
+    class InternManager:IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi.");
+        }
+    }
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+
+    }
+}
+/*
+ * inherits- class
+ * implements - interface
+ * interface new lenemez
+ */
